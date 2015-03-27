7bee3249a963d78a8e07c62e61a020ada1ee8444
Javier Scappini <jscappini@gmail.com>
2015-03-27 11:39:54 -0300

    Update index.js package.json

--- index.js
+++ index.js
@@ -17,48 +17,48 @@
   };
 
   processFolder = function(src, dest) {
-    var currentFile, file, fileExt, fileStats, files, _i, _len, _ref, _results;
+    var currentFile, file, fileExt, fileStats, files, i, len, ref, results;
     files = fs.readdirSync(src);
-    _results = [];
-    for (_i = 0, _len = files.length; _i < _len; _i++) {
-      file = files[_i];
-      currentFile = "" + src + "/" + file;
+    results = [];
+    for (i = 0, len = files.length; i < len; i++) {
+      file = files[i];
+      currentFile = src + "/" + file;
       fileStats = fs.statSync(currentFile);
       if (fileStats.isFile()) {
         fileExt = path.extname(currentFile);
-        if ((_ref = fileExt.toLowerCase()) === '.jpg' || _ref === '.jpeg') {
-          _results.push(saveFile(currentFile, file, dest));
+        if ((ref = fileExt.toLowerCase()) === '.jpg' || ref === '.jpeg') {
+          results.push(saveFile(currentFile, file, dest));
         } else {
-          _results.push(void 0);
+          results.push(void 0);
         }
       } else if (fileStats.isDirectory()) {
-        _results.push(processFolder(currentFile, dest));
+        results.push(processFolder(currentFile, dest));
       } else {
-        _results.push(void 0);
+        results.push(void 0);
       }
     }
-    return _results;
+    return results;
   };
 
   saveFile = function(fullFileName, fileName, destFolder) {
     return new ExifImage({
       image: fullFileName
     }, function(err, data) {
-      var date, dateArr, file, folder, imageDate, _ref, _ref1;
+      var date, dateArr, file, folder, imageDate, ref, ref1;
       if (!err) {
-        imageDate = ((_ref = data.exif) != null ? _ref.DateTimeOriginal : void 0) || ((_ref1 = data.image) != null ? _ref1.ModifyDate : void 0);
+        imageDate = ((ref = data.exif) != null ? ref.DateTimeOriginal : void 0) || ((ref1 = data.image) != null ? ref1.ModifyDate : void 0);
         if (imageDate != null) {
           date = imageDate.replace(/\ /g, ':');
           dateArr = date.split(':');
           if (dateArr.length > 5) {
-            folder = "" + destFolder + "/" + dateArr[0] + "/" + dateArr[1] + "/" + dateArr[2];
-            file = "" + dateArr[3] + "_" + dateArr[4] + "_" + fileName;
+            folder = destFolder + "/" + dateArr[0] + "/" + dateArr[1] + "/" + dateArr[2];
+            file = dateArr[3] + "_" + dateArr[4] + "_" + fileName;
             fs.mkdirsSync(folder);
             if (this.opt === '--move') {
-              fs.copySync(fullFileName, "" + folder + "/" + file);
+              fs.copySync(fullFileName, folder + "/" + file);
               return fs.removeSync(fullFileName);
             } else {
-              return fs.copySync(fullFileName, "" + folder + "/" + file);
+              return fs.copySync(fullFileName, folder + "/" + file);
             }
           }
         }
